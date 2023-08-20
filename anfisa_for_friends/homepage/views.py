509d2b3f690bb480af3cd93f7f5a5e4a8ef06b6a
Дмитрий <d77s@inbox.ru>
2023-08-20 12:58:36 +0300

    Next

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -5,6 +5,7 @@ from ice_cream.models import IceCream
 
 def index(request):
     template_name = 'homepage/index.html'
-    ice_cream_list = IceCream.objects.values('title', 'id', 'description')
+    ice_cream_list = IceCream.objects.values('title', 'id', 'description'
+                                             ).filter(is_on_main=True)
     context = {'ice_cream_list': ice_cream_list, }
     return render(request, template_name, context)
